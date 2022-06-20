ea82b9e6d0e9696146a8a42205f90b51d7a53ba2
realdarkxeon <realdarkxeon@gmail.com>
2022-06-20 14:16:45 +0600

    Finish ItemCard

--- src/components/item-card/index.jsx
+++ src/components/item-card/index.jsx
@@ -3,15 +3,17 @@ import "./item-card.css"
 
 export const ItemCard = ({cake}) => {
     return (
-        <div className="card cake-card h-100 mx-3">
+        <div className="card cake-card">
             <img className="card-img-top cake-photo" src={cake.url} />
             <div className="card-body">
                 <p className="card-title">{cake.name}</p>
                 <p className="card-text">{cake.description}</p>
-                <div className="cake-card-footer">
-                    <p className="cake-price">{cake.price}</p>
-                    <button className="add-to-cart">Add to Cart</button>
+            </div>
+            <div className="cake-card-footer">
+                <div class="price-section">
+                    <div className="cake-price">{cake.price}</div>
                 </div>
+                <button className="add-to-cart">Add to Cart</button>
             </div>
         </div>
     )
