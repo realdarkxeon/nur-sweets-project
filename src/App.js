f3b68a436e039cd9dcbd7f5dd1e7cecebff506e3
realdarkxeon <realdarkxeon@gmail.com>
2022-06-20 12:36:31 +0600

    Start work with component ItemCard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import { ItemCard } from './components/item-card';
+
+const Cakes = [
+  {
+      url: "https://static.tildacdn.com/tild3331-3463-4665-a233-653066363037/-51.JPG",
+      name: "Milky Girl",
+      price: 5800,
+      description: "Air biscuits and buttercream 'plombir'",
+  },
+  {
+      url: "https://static.tildacdn.com/tild3761-3234-4836-b062-663539323563/-98.JPG",
+      name: "Red Velvet",
+      price: 6500,
+      description: "Moist, porous biscuits and cream cheese. sgjndfgjkbgjbflgjbziudghluigblidghkbgjlbdlgfbhzdgzfbgbzdgbjhgfbsgjndfgjkbgjbflgjbziudghluigblidghkbgjlbdlgfbhzdgzfbgbzdgbjhgfbsgjndfgjkbgjbflgjbziudghluigblidghkbgjlbdlgfbhzdgzfbgbzdgbjhgfbsgjndfgjkbgjbflgjbziudghluigblidghkbgjlbdlgfbhzdgzfbgbzdgbjhgfbsgjndfgjkbgjbflgjbziudghluigblidghkbgjlbdlgfbhzdgzfbgbzdgbjhgfb",
+  }
+];
 
 function App() {
   return (
-    <div>
-      Hello, World!
+    <div className="row">
+      {Cakes.map((cake) => (
+        <ItemCard cake={cake}/>
+      ))}
     </div>
   );
 }
--- src/components/item-card/index.jsx
+++ src/components/item-card/index.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import "./item-card.css"
 
-export const ItemCard = () => {
+export const ItemCard = ({cake}) => {
     return (
-        <div>
-            
+        <div className="card cake-card h-100 mx-3">
+            <img className="card-img-top cake-photo" src={cake.url} />
+            <div className="card-body">
+                <p className="card-title">{cake.name}</p>
+                <p className="card-text">{cake.description}</p>
+                <div className="cake-card-footer">
+                    <p className="cake-price">{cake.price}</p>
+                    <button className="add-to-cart">Add to Cart</button>
+                </div>
+            </div>
         </div>
     )
 };
